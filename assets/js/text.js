88cf56bc7c9c73848471b1f0ee2d047351acf97a
Child <77742611+Lowong1337@users.noreply.github.com>
2023-10-16 22:31:01 +0700

    123

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,34 +1,48 @@
 $(document).ready(function() {
-    const title = document.title;
-    const animSeq = ["|"];
-    let animIndex = 0;
-    let titleIndex = 0;
-  
-    function doInverseSpinZeroPitch() {
+  const title = document.title;
+  const animSeq = ["|"];
+  let animIndex = 0;
+  let titleIndex = 0;
+
+  function doInverseSpinZeroPitch() {
       const loadTitle = title.substring(0, titleIndex);
-  
+
       if (titleIndex > title.length) {
-        animIndex = 0;
-        titleIndex = 0;
+          animIndex = 0;
+          titleIndex = 0;
       }
-  
+
       if (animIndex > 1) {
-        titleIndex++;
-        animIndex = 0;
+          titleIndex++;
+          animIndex = 0;
       }
-  
+
       document.title = loadTitle + animSeq[0];
       animIndex++;
-    }
-  
-    setInterval(doInverseSpinZeroPitch, 150);
-  });
-  
-  $(document).ready(function() {
-    $(".typed").typed({
-      strings: ["why are you here?", "btw, here is my QQ 3065705698", "but im not chinese", "feeling kinda unwell but", "she likes winter melon tea", "❤️❤️❤️❤️❤️"],
+  }
+
+  setInterval(doInverseSpinZeroPitch, 150);
+});
+
+$(document).ready(function() {
+  const strings = [
+      "she likes winter melon tea",
+      "❤️❤️❤️❤️❤️"
+  ];
+
+  function shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+  }
+
+  shuffleArray(strings);
+
+  $(".typed").typed({
+      strings: strings,
       typeSpeed: 50,
       loop: true,
       cursorChar: "|",
-    });
   });
+});
